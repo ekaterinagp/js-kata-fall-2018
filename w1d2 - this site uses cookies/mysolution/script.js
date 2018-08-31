8ecc3eaca74a04d7b5dc84cf5e5c26b89f3096c4
ekaterinagp <katya.koriakina@gmail.com>
2018-08-31 13:12:43 +0200

    functionality - done

--- w1d2 - this site uses cookies/mysolution/script.js
+++ w1d2 - this site uses cookies/mysolution/script.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let webAlert = document.querySelector("div");
 let btnHide = document.querySelector("button");
@@ -6,17 +6,18 @@ let btnHide = document.querySelector("button");
 function showAlert() {
   webAlert.classList.add("slide");
 }
+
+//showAlert();
 // ES5
 setTimeout(function() {
   showAlert();
-});
+}, 100);
+
 // ES6
 // setTimeout(() => {
 //   showAlert();no need for number? (something to do with loading?)
 // });
 
-
-
 btnHide.addEventListener("click", function() {
-  webAlert.classList.remove("slide")
+  webAlert.classList.remove("slide");
 });
