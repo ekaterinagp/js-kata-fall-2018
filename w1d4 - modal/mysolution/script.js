721e40ed23336c029278067043839b139b331c55
ekaterinagp <katya.koriakina@gmail.com>
2018-08-31 13:12:17 +0200

    functionality is done

---
+++ w1d4 - modal/mysolution/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+let btnDetails = document.querySelectorAll(".details");
+let modal = document.querySelector("#modal_container");
+let closeBtn = document.querySelector("span");
+
+btnDetails.forEach(function(e) {
+  e.addEventListener("click", function() {
+    modal.classList.remove("hidden")
+  });
+
+  closeBtn.addEventListener("click", function() {
+    modal.classList.add("hidden")
+  });
+})
+
+
+
+// function showModal() {
+//   console.log("Show modal");
+
+
+// }
+
+
+// function hideModal() {
+//   console.log("Hide modal");
+// }
