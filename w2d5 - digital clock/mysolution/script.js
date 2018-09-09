c9a261cb0b177b3609f2f1f31472cbab62142a86
ekaterinagp <katya.koriakina@gmail.com>
2018-09-09 21:08:45 +0200

    done

--- w2d5 - digital clock/mysolution/script.js
+++ w2d5 - digital clock/mysolution/script.js
@@ -4,50 +4,34 @@ let hoursSpan = document.querySelector(".hours");
 let minutesSpan = document.querySelector(".minutes");
 let secondsSpan = document.querySelector(".seconds");
 
-function convertTime(time, spanElement) {
-  let allElements = spanElement.querySelectorAll('span');
-  allElements.forEach((element) => {
-    element.classList.add("hide");
-  })
-  // console.log(allElements);
-  console.log('time: ', time);
-  console.log('spanElement: ', spanElement);
-
+function convertTime(time, spanElement, name) {
+  spanElement.querySelector("#" + name + "1").classList = '';
+  spanElement.querySelector("#" + name + "2").classList = '';
 
   if (time < 10) {
-    spanElement.querySelector(".digit0").classList.remove("hide");
-
-    if (time === 0) {
-      spanElement.querySelectorAll(".digit" + time)[1].classList.remove("hide");
-    } else {
-      spanElement.querySelector(".digit" + time).classList.remove("hide");
-    }
+    spanElement.querySelector("#" + name + "1").classList.add("digit0");
+    spanElement.querySelector("#" + name + "1").classList.add("digit");
+    spanElement.querySelector("#" + name + "2").classList.add("digit" + time);
+    spanElement.querySelector("#" + name + "2").classList.add("digit");
   } else {
 
     let timeString = time.toString();
     let timeArray = timeString.split('');
     console.log(timeArray);
-
-    if (timeArray[0] == timeArray[1]) {
-      spanElement.querySelectorAll(".digit" + timeArray[0])[0].classList.remove("hide");
-      spanElement.querySelectorAll(".digit" + timeArray[0])[1].classList.remove("hide");
-      console.log('I am same digits: time: ', time);
-    } else {
-      console.log('I am 2 digits, above 10 and not same: ', time);
-      spanElement.querySelector(".digit" + timeArray[0]).classList.remove("hide");
-      spanElement.querySelector(".digit" + timeArray[1]).classList.remove("hide");
-    }
+    spanElement.querySelector("#" + name + "1").classList.add("digit");
+    spanElement.querySelector("#" + name + "2").classList.add("digit");
+    spanElement.querySelector("#" + name + "1").classList.add("digit" + timeArray[0]);
+    spanElement.querySelector("#" + name + "2").classList.add("digit" + timeArray[1]);
   }
-
 }
 
 let date = new Date();
 let hours = date.getHours();
 let minutes = date.getMinutes();
 let seconds = date.getSeconds();
-convertTime(hours, hoursSpan);
-convertTime(minutes, minutesSpan);
-convertTime(seconds, secondsSpan);
+convertTime(hours, hoursSpan, "hours");
+convertTime(minutes, minutesSpan, "minutes");
+convertTime(seconds, secondsSpan, "seconds");
 
 setInterval(() => {
 
@@ -55,8 +39,7 @@ setInterval(() => {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
-
-  convertTime(hours, hoursSpan);
-  convertTime(minutes, minutesSpan);
-  convertTime(seconds, secondsSpan);
+  convertTime(hours, hoursSpan, "hours");
+  convertTime(minutes, minutesSpan, "minutes");
+  convertTime(seconds, secondsSpan, "seconds");
 }, 1000)
