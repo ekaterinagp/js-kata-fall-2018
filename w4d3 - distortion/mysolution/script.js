d58788f2d70529f780ec6f66b0337377eb94a567
ekaterinagp <katya.koriakina@gmail.com>
2018-09-19 18:33:11 +0200

    added style

--- w4d3 - distortion/mysolution/script.js
+++ w4d3 - distortion/mysolution/script.js
@@ -1,6 +1,5 @@
 "use strict";
 
-
 let endpoint = "https://kea-alt-del.dk/kata-distortion/";
 // let template = document.querySelector("template").content;
 let queue = document.querySelector("svg");
@@ -10,14 +9,15 @@ let barName = document.querySelector(".barName");
 let empName = document.querySelector(".employee");
 let orderNow = document.querySelector(".orderNow");
 let peopleNumberNow = document.querySelector(".peopleNumber");
+let weBusy = document.querySelector(".weAreBusy");
 
 function queueUpdate() {
-  fetch(endpoint).then(e => e.json())
+  fetch(endpoint)
+    .then(e => e.json())
     .then(showQueue)
     .catch(error => {
       console.log("fetch: error: ", error);
-    })
-
+    });
 }
 setInterval(function() {
   queueUpdate();
@@ -31,6 +31,12 @@ init();
 
 function showQueue(peopleinQueue) {
   console.log("showQueue is called. people :", peopleinQueue);
+
+  if (lineArray.length > 10) {
+    queue.innerHTML = " ";
+    lineArray = [];
+  }
+
   barName.textContent = peopleinQueue.name;
   empName.textContent = peopleinQueue.personel;
   peopleNumberNow.textContent = peopleinQueue.inQueue;
@@ -42,33 +48,28 @@ function showQueue(peopleinQueue) {
 
   if (lineArray.length <= 1) {
     rect.setAttribute("x", 0);
-
   } else {
     rect.setAttribute("x", (lineArray.length - 1) * 8);
-
   }
   rect.setAttribute("y", 100 - Number(peopleinQueue.inQueue));
   rect.setAttribute("width", 6);
-  rect.setAttribute("height", 100 + Number(peopleinQueue.inQueue));
+  rect.setAttribute("height", Number(peopleinQueue.inQueue));
   rect.classList.add(".trans");
-  if (peopleinQueue.inQueue < 5) {
+  if (peopleinQueue.inQueue < 11) {
     rect.style.fill = "green";
     orderNow.classList.add("show");
   } else {
     rect.style.fill = "red";
     orderNow.classList.remove("show");
   }
+  if (peopleinQueue.inQueue > 18) {
+    weBusy.classList.add("show");
+  } else {
+    weBusy.classList.remove("show");
+  }
   // let clone = template.cloneNode(true);
   // clone.querySelector(".barName").textContent = peopleinQueue.name;
   // clone.querySelector(".inLine").textContent = peopleinQueue.inQueue;
   // clone.querySelector(".loggedTime").textContent = peopleinQueue.loggedAt;
   queue.appendChild(rect);
-
-  if (lineArray.length > 8) {
-    queue.innerHTML = " ";
-    lineArray = [];
-    clearInterval();
-
-  }
-
 }
