86586e6064682854d66b04e5dcb70dea0369c522
ekaterinagp <katya.koriakina@gmail.com>
2018-09-19 12:57:44 +0200

    bar chart without animation or text

--- w4d3 - distortion/mysolution/script.js
+++ w4d3 - distortion/mysolution/script.js
@@ -2,8 +2,10 @@
 
 
 let endpoint = "https://kea-alt-del.dk/kata-distortion/";
-let template = document.querySelector("template").content;
-let queue = document.querySelector("section");
+// let template = document.querySelector("template").content;
+let queue = document.querySelector("svg");
+const NS = "http://www.w3.org/2000/svg";
+let lineArray = [];
 
 function queueUpdate() {
   fetch(endpoint).then(e => e.json())
@@ -15,20 +17,47 @@ function queueUpdate() {
 }
 setInterval(function() {
   queueUpdate();
-}, 11000);
+}, 10000);
 
 function init() {
-  window.addEventListener("DOMContentLoaded", queueUpdate(), false);
+  window.addEventListener("DOMContentLoaded", queueUpdate());
 }
 
 init();
 
 function showQueue(peopleinQueue) {
   console.log("showQueue is called. people :", peopleinQueue);
-  let clone = template.cloneNode(true);
-  clone.querySelector(".barName").textContent = peopleinQueue.name;
-  clone.querySelector(".inLine").textContent = peopleinQueue.inQueue;
-  clone.querySelector(".loggedTime").textContent = peopleinQueue.loggedAt;
-  queue.appendChild(clone);
+  // let peopleNumber = Number(peopleinQueue.inQueue);
+  // console.log("to number :", peopleNumber);
+  lineArray.push(peopleinQueue);
+  const rect = document.createElementNS(NS, "rect");
+
+  if (lineArray.length <= 1) {
+    rect.setAttribute("x", 0);
+
+  } else {
+    rect.setAttribute("x", (lineArray.length - 1) * 8);
+
+  }
+  rect.setAttribute("y", 100 - Number(peopleinQueue.inQueue));
+  rect.setAttribute("width", 6);
+  rect.setAttribute("height", 100 + Number(peopleinQueue.inQueue));
+  if (peopleinQueue.inQueue < 10) {
+    rect.style.fill = "green";
+  } else {
+    rect.style.fill = "red";
+  }
+  // let clone = template.cloneNode(true);
+  // clone.querySelector(".barName").textContent = peopleinQueue.name;
+  // clone.querySelector(".inLine").textContent = peopleinQueue.inQueue;
+  // clone.querySelector(".loggedTime").textContent = peopleinQueue.loggedAt;
+  queue.appendChild(rect);
+
+  if (lineArray.length > 8) {
+    queue.innerHTML = " ";
+    lineArray = [];
+    clearInterval();
+
+  }
 
 }
